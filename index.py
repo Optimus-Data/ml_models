05a0ff67c5e28108d407fdf2dc9216e790d2416f
Matheus Lopes Ferreira <lopesferreira.matheus9@gmail.com>
2025-05-27 19:24:11 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,3 +1,7 @@
+// Chatbot da Câmara Municipal de São Paulo
+// (c) 2025 Optimus Data Technology - Uso permitido apenas para estudos
+// Contato: claudinei.goncalves@optimusdata.com.br | (11) 98185-5447
+
 import uvicorn
 import os
 from dotenv import load_dotenv
