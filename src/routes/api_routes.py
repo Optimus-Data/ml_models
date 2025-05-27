448631336d34426991d5f295d1ff37a034d3835b
Matheus Lopes Ferreira <lopesferreira.matheus9@gmail.com>
2025-05-27 19:25:54 -0300

    Update api_routes.py

--- src/routes/api_routes.py
+++ src/routes/api_routes.py
@@ -1,6 +1,6 @@
-// Chatbot da Câmara Municipal de São Paulo
-// (c) 2025 Optimus Data Technology - Uso permitido apenas para estudos
-// Contato: claudinei.goncalves@optimusdata.com.br | (11) 98185-5447
+# Chatbot da Câmara Municipal de São Paulo
+# (c) 2025 Optimus Data Technology - Uso permitido apenas para estudos
+# Contato: claudinei.goncalves@optimusdata.com.br | (11) 98185-5447
 
 from fastapi import APIRouter, Depends
 from src.schemas.text_input import TextInput
