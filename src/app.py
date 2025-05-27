9cf598137036e3cfd0d302e0eb340613a074d484
Matheus Lopes Ferreira <lopesferreira.matheus9@gmail.com>
2025-05-27 19:24:23 -0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,3 +1,7 @@
+// Chatbot da Câmara Municipal de São Paulo
+// (c) 2025 Optimus Data Technology - Uso permitido apenas para estudos
+// Contato: claudinei.goncalves@optimusdata.com.br | (11) 98185-5447
+
 from fastapi import FastAPI
 from src.routes.api_routes import router 
 from src.dependencies.dependencies import AuthMiddleware 
